af5d3547283be2215622ab11f4cd00dbd3780ca5
Ricardo Souto <ricardo.souto@ons.org.br>
2019-04-11 15:06:24 -0300

    Including class models

--- core/models.py
+++ core/models.py
@@ -3,14 +3,29 @@ from django.db import models
 
 class Solution(models.Model):
     """
-    solution model
+    Solution model. This is the main solution to which the platform will serve. (SAGER)
     """
     name = models.CharField(max_length=30)
-
+    
 
 class EntityModel(models.Model):
     """
-    entity model
+    Entity model. This is the Entity that will be used in the solution. (USINA)
     """
     name = models.CharField(max_length=30)
     solution = models.ForeignKey(Solution, on_delete=None)
+
+class App(models.Model):
+    """
+    app model
+    """
+    name = models.CharField(max_length=30)
+    solution = models.ForeignKey(Solution, on_delete=None)
+
+class Field(models.Model):
+    """
+    field model
+    """
+    name = models.CharField(max_length=30)
+    typeName = models.CharField(max_length=30)
+    entityModel = models.ForeignKey(EntityModel, on_delete=None)
