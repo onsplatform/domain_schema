dc1c29724827656fbbd245e38e4ded6b7cdcf403
Ricardo Souto <ricardo.souto@ons.org.br>
2019-06-10 19:55:51 -0300

    Open file, create and save entity and fields

--- core/management/commands/import_data.py
+++ core/management/commands/import_data.py
@@ -1,26 +1,65 @@
 from django.core.management.base import BaseCommand
 from os import walk, path
-import yaml
-import io
+import yaml, io
 from glob import glob
 from typing import List
 
+from core.models import Entity, Field
+
 class Command(BaseCommand):
     def handle(self, **options):
-        # open folder and load yaml files list within it.
+        """
+        Dictionary sample the code above will import.
+            {'e_ageoper': 
+                {
+                    'id_age': ['string'], 
+                    'ido_ons': ['string'], 
+                    'nom_curto': ['string'], 
+                    'dat_entrada': ['datetime'], 
+                    'dat_desativacao': ['datetime'], 
+                    'nom_longo': ['string']
+                }
+            }
+        """
+        # open folder and list all yaml files within it.
         yamlList = self.listYamlFiles('./domain_schema/core/management/commands/')
 
         # if there are yaml files at path
         if yamlList:
-            # open each file, and load it´s contents. FullLoader is safe.
+            # for each file in folder...
             for file in yamlList:
-                with open(file, 'r', enconding='utf-8') as stream:
-                    yamlFile = yaml.load(stream,Loader=yaml.FullLoader)
-                    # ... record entity
+                try:
+                    entityName = ''
+                    fields = {}
+                    myEntity = Entity()
+                    myFields = Field()
+                    
+                    # Open file, read it, populate variables, close file.
+                    # Perhaps we should extract a method here...
+                    with open(file, 'r', encoding='utf-8') as stream:
+                        yamlDict = yaml.load(stream,Loader=yaml.FullLoader)
 
-        
-        # ler yaml
-        # colocar num dict
+                        for data in yamlDict.items():
+                            entityName = data[0]
+                            fields = data[1]
+
+                    # Create Entity and link Fields to it.
+                    myEntity.name = entityName
+                    myFields.entity = entityName                                          
+                    
+                    # We have to review this loop
+                    for k,v in fields.item():
+                        myFields.name = k
+                        myFields.field_type = v
+                        myFields.save()
+                    
+                    myEntity.save()
+
+                except OSError:
+                    return "Program was not able to open file at given destination"
+                    
+        # ler yaml - ok
+        # colocar num dict - ok
         # ex:
         # data = [
         # {
