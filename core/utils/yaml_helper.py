e18502804880ecc8f12c052b600ec2443d350db4
Ricardo Souto <ricardo.souto@ons.org.br>
2019-06-14 17:46:28 -0300

    Created a Yaml helper.

---
+++ core/utils/yaml_helper.py
@@ -0,0 +1,12 @@
+from os import path
+from glob import glob
+from typing import List
+
+
+def list_files(filepath: str) -> List:
+    """ Returns a list of YAML files in a given path. """
+    # check if path exists
+    if path.exists(filepath):
+        yaml_files = glob(filepath + '*.yaml')
+        assert isinstance(yaml_files, List)
+        return yaml_files
