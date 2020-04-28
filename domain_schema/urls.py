3f8802ca3ec912568091e18266d9fe98be5d3ae3
Leitao <uncodead@gmail.com>
2020-04-28 19:45:31 -0300

    query para obter as tags e tabelas reprocessaveis

--- domain_schema/urls.py
+++ domain_schema/urls.py
@@ -3,7 +3,7 @@ from django.urls import path, include
 from rest_framework import routers
 
 from core.router import router as core_router
-from core.views import CreateEntityView, CreateMapMapView
+from core.views import CreateEntityView, CreateMapMapView, AppVersionByReprocessableEntityView
 
 api_version = 'api/v1/'
 
@@ -12,5 +12,6 @@ urlpatterns = [
     path(api_version, include(core_router.urls)),
     path(api_version + 'create/entity/', CreateEntityView.as_view()),
     path(api_version + 'create/map/', CreateMapMapView.as_view()),
+    path(api_version + 'appversion/byreprocessableentities', AppVersionByReprocessableEntityView.as_view())
 ]
 
